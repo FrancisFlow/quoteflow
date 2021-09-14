9b0bfde81d2945e484dcb5f17fa04b076ca177f4
FrancisFlow <iamngigikaranja1@aol.com>
2021-09-14 13:40:57 +0300

    Filled in values of upVote and downvotes Created toggleDetails function to reverse the value of showiSubmit

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -7,16 +7,21 @@ import { Quote } from '../quote';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
-  
+
   quotes:Quote[]= [
-    new Quote("The only limit to our realization of tomorrow will be our doubts today", "Franklin D. Rosavlete", "Daniel Roth"),
-    new Quote("Some people look for a beautiful place. Others make a beautiful place", "~anonymous", "Kennedy Kamiro"),
-    new Quote("Keep your face to the sunshine and you cannot see a shadow", "Hellen Keller", "Walter Whitman"),
-    new Quote("Nobody ever wrote a plan to be broke, fat, lazy or stupid. those things ae what happen when you don't have a plan", "Larry Winget", "Michelle Muthoni"),
-    new Quote("A single grain of rice can tip the scale", "Emperor", "Faith Kamau"),
-    new Quote("No matter how the wind howls, the mountain cannot bow to it", "Mula", "Disney Okumu")
+    new Quote("The only limit to our realization of tomorrow will be our doubts today", "Franklin D. Rosavlete", "Daniel Roth", 0, 0),
+    new Quote("Some people look for a beautiful place. Others make a beautiful place", "~anonymous", "Kennedy Kamiro", 0, 0),
+    new Quote("Keep your face to the sunshine and you cannot see a shadow", "Hellen Keller", "Walter Whitman", 0, 0),
+    new Quote("Nobody ever wrote a plan to be broke, fat, lazy or stupid. those things ae what happen when you don't have a plan", "Larry Winget", "Michelle Muthoni", 0, 0),
+    new Quote("A single grain of rice can tip the scale", "Emperor", "Faith Kamau", 0, 0),
+    new Quote("No matter how the wind howls, the mountain cannot bow to it", "Mula", "Disney Okumu", 0, 0)
   ];
 
+  // toggle between show and hide description
+
+  toggleDetails(index: number) {
+    this.quotes[index].showiSubmit= !this.quotes[index].showiSubmit;
+  }
 
   constructor() { }
 
