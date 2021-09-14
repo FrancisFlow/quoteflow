bfb272170759712b557919718d1a65344bf9a393
FrancisFlow <iamngigikaranja1@aol.com>
2021-09-14 11:43:34 +0300

    Created quote-details component and performed input property binding to it to recieve data from the parent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeadSectionComponent } from './head-section/head-section.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeadSectionComponent,
-    QuotesComponent
+    QuotesComponent,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule,
