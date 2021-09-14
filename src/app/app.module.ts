b3998d14fe4f3197f8793b55a6006589e5b1671f
FrancisFlow <iamngigikaranja1@aol.com>
2021-09-14 15:49:33 +0300

    Imported the formsmodule from angular forms and added formsmodule to imports array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeadSectionComponent } from './head-section/head-section.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { HighlightHighestVoteDirective } from './highlight-highest-vote.directive';
+import { NewQuoteFormComponent } from './new-quote-form/new-quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeadSectionComponent,
     QuotesComponent,
-    QuoteDetailsComponent
+    QuoteDetailsComponent,
+    HighlightHighestVoteDirective,
+    NewQuoteFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
