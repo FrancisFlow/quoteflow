6be87459141eeb686a678bafc045056fd826bb43
FrancisFlow <iamngigikaranja1@aol.com>
2021-09-16 11:53:59 +0300

    Created logic for pipe and applied it to dayOfPosting property

--- src/app/days-passed.pipe.ts
+++ src/app/days-passed.pipe.ts
@@ -6,8 +6,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DaysPassedPipe implements PipeTransform {
 
   transform(value: any): number {
-    let today:Date= new Date();
-    
+   //today-dayPosted
+
+    let today:any = new Date();
+    let dateDifference = Math.abs(Date.parse(today)-Date.parse(value))*0.001;
+    let differenceInDays= dateDifference/(60*60*24);
+    return Math.floor(differenceInDays);
+  
+
+
   }
 
 }
